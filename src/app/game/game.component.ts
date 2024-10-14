26575d8f976c5264d4ab0bc0d624515dccced32e
Christoph Völker <christoph@herrvoelker.de>
2024-10-14 10:47:46 +0200

    update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+
+  constructor() { }
+
+  pickCardAnimation = false;
+
+  ngOnInit(): void {
+
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+
 
 }
